3195ee9b551f1397ead71252ae98462249ccc63e
YudinSergey <serg93.12@mail.ru>
2020-08-10 21:32:36 +0300

    Updates

---
+++ precache-manifest.7c94c88feb9af500e1c74c1ef32e8a4e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487d6207b1360c94c15b8df4e546efc6",
+    "url": "/newmuztorg/index.html"
+  },
+  {
+    "revision": "52b37d10db0c8eeb5136",
+    "url": "/newmuztorg/static/css/main.462726c1.chunk.css"
+  },
+  {
+    "revision": "27ea0b2e2f5bf65db0d8",
+    "url": "/newmuztorg/static/js/2.13a56b28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newmuztorg/static/js/2.13a56b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b37d10db0c8eeb5136",
+    "url": "/newmuztorg/static/js/main.ed870252.chunk.js"
+  },
+  {
+    "revision": "72ca04436633f4dda1b5",
+    "url": "/newmuztorg/static/js/runtime-main.33e19fcf.js"
+  },
+  {
+    "revision": "d3b431f14cf58d6a95279e91e7cf1bfb",
+    "url": "/newmuztorg/static/media/famousMusicians.d3b431f1.jpg"
+  },
+  {
+    "revision": "b0f12905143eb2680fcc42fe8d14a597",
+    "url": "/newmuztorg/static/media/firstShare.b0f12905.jpg"
+  },
+  {
+    "revision": "43261a8835745dd7fc0547cd08fe4515",
+    "url": "/newmuztorg/static/media/logo_muztorg.43261a88.svg"
+  },
+  {
+    "revision": "f4c7f21351090719e6d378e147a83abd",
+    "url": "/newmuztorg/static/media/muztorg-background.f4c7f213.jpg"
+  },
+  {
+    "revision": "778565986e6534fb5b5b8b7b886e45dc",
+    "url": "/newmuztorg/static/media/muztorg-background2.77856598.jpg"
+  },
+  {
+    "revision": "9aad67a8dcddc357fb71c4cea4ae288a",
+    "url": "/newmuztorg/static/media/secondProduct.9aad67a8.jpg"
+  },
+  {
+    "revision": "2a841254016faca378de6b2e308a46a7",
+    "url": "/newmuztorg/static/media/secondShare.2a841254.jpg"
+  },
+  {
+    "revision": "88274993e96f6f4914050348571d7e77",
+    "url": "/newmuztorg/static/media/thirdProduct.88274993.jpg"
+  },
+  {
+    "revision": "b8ebbff75c5a768c59c284fcc521d6cd",
+    "url": "/newmuztorg/static/media/thirdShare.b8ebbff7.jpg"
+  }
+]);
